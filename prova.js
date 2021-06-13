a189b86a76b54e6f384122390f3e76e26ad66162
mauri1103 <81741618+mauri1103@users.noreply.github.com>
2021-06-13 20:12:36 +0200

    inizio corso TypeScript Course

---
+++ prova.js
@@ -0,0 +1,9 @@
+console.log("Test console");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var numero1 = 5;
+var numero2 = 1.75;
+var testo = "Il sisultato finale e il seguente";
+var result = add(numero1, numero2);
+console.log(testo + " " + result);
---
+++ prova.ts
@@ -0,0 +1,14 @@
+console.log("Test console");
+
+function add(n1:number,n2:number){
+    return n1+n2;
+}
+const numero1 = 5;
+const numero2 = 1.75;
+const testo = "Il sisultato finale e il seguente";
+
+
+const result = add(numero1, numero2);
+
+
+console.log(testo + " " + result);
