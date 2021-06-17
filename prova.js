6a55f41cb818bfc8765f2daaf2f6f0fe7d24bf92
mauri1103 <81741618+mauri1103@users.noreply.github.com>
2021-06-17 15:17:36 +0200

    aggiunti enum

--- prova.js
+++ prova.js
@@ -1,3 +1,10 @@
+var Ruoli;
+(function (Ruoli) {
+    Ruoli[Ruoli["ADMIN"] = 0] = "ADMIN";
+    Ruoli[Ruoli["USER"] = 1] = "USER";
+    Ruoli[Ruoli["AUTORE"] = 2] = "AUTORE";
+})(Ruoli || (Ruoli = {}));
+; //come creare un enum 
 var persona = 
 // : {
 //   nickName: string;
@@ -9,6 +16,20 @@ var persona =
     nickName: "Superman",
     nome: "Robert",
     cognome: "Basics",
-    eta: 30
+    eta: 30,
+    hobbie: ["gym", "nuoto"],
+    ruolo: [2, 'autore'],
+    role: Ruoli.ADMIN
 };
+// persona.ruolo.push('admin'); // aggiungo il ruolo admin
+// persona.ruolo[1]=10;
+var attivitaFavorite;
+attivitaFavorite: ["calcio"];
 console.log(persona.nome);
+for (var _i = 0, _a = persona.hobbie; _i < _a.length; _i++) {
+    var h = _a[_i];
+    console.log(h);
+}
+if (persona.role === Ruoli.ADMIN) {
+    console.log('is admin');
+}
