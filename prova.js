154ee7df5d1b8f451a1b27106cce84ed4086ce1d
mauri1103 <81741618+mauri1103@users.noreply.github.com>
2021-06-20 18:07:49 +0200

    implementato gli errori

--- prova.js
+++ prova.js
@@ -1,23 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput; // e simile a any, si possono passare tipi diversi di valori, pero e molto piu ristretto a differnza di any che e piu versatile
+var userName;
+userInput = 5;
+userInput = "Roberto";
+// a userName non gli posso passare userInpute se pure e di tipo unknown,
+// perche e non e garantito che sia di tipo stringa, a differenza di any che e molto piu versatile e disattiva 'il check' dei tipi
+// userName = userInput;
+//con il seguent if vado a controllorare se realmente e una stringa e si corregge l'errore del unknown
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-//la seguente funzione ha come ritorno void e non e obbligatorio mettere il return
-function printResult(num) {
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-// nella seguente fuznione vado ad assegnare un call back con tipolia void e associando il tipo del  paramentro assegnato
-function addAndHandler(n1, n2, cb) {
-    var risultato = n1 + n2;
-    cb(risultato);
-}
-printResult(add(5, 12));
-var combineValues; // alla variabiele gli associo il tipo funzione cosi in seguito gli posso passare solo quel tipo
-var combineValuesOk; // creo una mini funzione che va a sitemare il bug di non riconoscere e sommare i valori 
-combineValues = add;
-combineValuesOk = add;
-// combineValues = printResult; // come risultato da undefined perche e di tipo void
-// console.log(combineValues(8, 8)); // ha un piccolo bug riconosce solo un singolo valore 
-console.log(combineValuesOk(8, 8));
-addAndHandler(10, 20, function (result) {
-    console.log(result);
-});
+generateError('Errore non riconosciuto', 500);
