7a78deaf17c76b3e329a7496b8b8dc50d54c86aa
mauri1103 <81741618+mauri1103@users.noreply.github.com>
2021-06-20 21:09:20 +0200

    prova expressjs

--- dist/src/prova.js
+++ dist/src/prova.js
@@ -1,1 +1,12 @@
-"use strict";
+import { Express } from "express-serve-static-core";
+
+const app = Express();
+const port = 3000;
+
+app.get('/', (req, res)=> {
+    res.send('hello world');
+});
+
+app.listen(port, ()=> console.log('la porta che sto usando' + port));
+
+
